30ee47d19692a3aba05852f5d2d127ffff18cf9e
Key Feula <keyfeula1214@gmail.com>
2024-04-01 15:36:07 -0700

    Write function playRound that plays one round of the game based on user and computer choices and return result as string

--- script.js
+++ script.js
@@ -16,3 +16,34 @@ function getComputerChoice () {
     }
     return computerChoice;
 }
+
+function playRound(computerSelection, playerSelection) {
+    //Format player selection string to match the computer selection string
+    playerSelection = playerSelection.at(0).toUpperCase() + playerSelection.substring(1).toLowerCase();
+    let result;
+
+    //Determine result of round based on player choice vs computer choice
+    if (playerSelection === computerSelection) {
+        result = "Draw!";
+    }
+    else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        result = "You win! Rock beats Scissors";
+    }
+    else if (playerSelection === "Rock" && computerSelection === "Paper") {
+        result = "You lose! Paper beats Rock";
+    }
+    else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        result = "You win! Scissors beats Paper";
+    }
+    else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+        result = "You lose! Rock beats Scissors";
+    }
+    else if (playerSelection === "Paper" && computerSelection === "Rock") {
+        result = "You win! Paper beats Rock";
+    }
+    else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+        result = "You lose! Scissors beats Paper";
+    }
+
+    return result;
+}
