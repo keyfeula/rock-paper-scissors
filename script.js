9f5da92dea3c67a16f15528ae1d29313cc585444
Key Feula <keyfeula1214@gmail.com>
2024-04-01 15:04:18 -0700

    Write function getComputerChoice that randomly returns one of the string: 'Rock', 'Paper', or 'Scissors'

---
+++ script.js
@@ -0,0 +1,18 @@
+function getComputerChoice () {
+    //Generates a random number between 1-3 (inclusive)
+    let randomNum = Math.floor((Math.random() * 3) + 1);
+    let computerChoice;
+    
+    switch (randomNum) {
+        case 1:
+            computerChoice = "Rock";
+            break;
+        case 2:
+            computerChoice = "Paper";
+            break;
+        case 3:
+            computerChoice = "Scissors";
+            break;
+    }
+    return computerChoice;
+}
